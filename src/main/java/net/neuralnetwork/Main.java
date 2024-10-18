e6d3fd90fc4ca2419c9f06482bf65992e8c94d31
Daniel Park <intheimageofyeshua@gmail.com>
2024-10-18 21:30:38 +0900

    And Gate

--- src/main/java/net/neuralnetwork/Main.java
+++ src/main/java/net/neuralnetwork/Main.java
@@ -3,36 +3,38 @@ package net.neuralnetwork;
 import static java.lang.System.out;
 
 /*
-*    INPUT  AND   OR   XOR   NOR   NAND   XNOR
-*     00     0    0     0     1      1      1
-*     01     0    1     1     0      1      0
-*     10     0    1     1     0      1      0
-*     11     1    1     0     0      0      1
+ *    INPUT  AND   OR   XOR   NOR   NAND   XNOR
+ *     00     0    0     0     1      1      1
+ *     01     0    1     1     0      1      0
+ *     10     0    1     1     0      1      0
+ *     11     1    1     0     0      0      1
  */
 
 public class Main {
-    public static void main(String[] args) {
-        // inputs
-        double[] x =  {0, 1};
-
-        // weights
-        double[] w = {0.5, 0.5};
-
-        // bias
-        double b = 0.5;
-
+    static double neuron( double[] x, double[] w, double b ) {
         // weighted sum
         double z = 0.0;
 
         for (int i = 0; i < x.length; i++) {
             z += x[i] * w[i];
         }
-         z += b;
+        z += b;
 
         // activation function
-        double a = z > 0 ? 1.0 : 0.0;
+        return z > 0 ? 1.0 : 0.0;
+    }
 
-        // print activation [output]
-        out.println(a);
+    static double and(double x1, double x2) {
+        return neuron(new double[] {x1, x2}, new double[] {1, 1}, -1);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+           double x1 = i / 2;
+           double x2 = i % 2;
+
+           double output = and(x1, x2);
+           out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);
+        }
     }
 }
