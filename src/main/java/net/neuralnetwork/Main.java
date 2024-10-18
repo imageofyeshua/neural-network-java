6d6a9aeecc15c8640db7b6472b4aea17b1ceafb1
Daniel Park <intheimageofyeshua@gmail.com>
2024-10-18 20:22:02 +0900

    Logic Gate Comment

--- src/main/java/net/neuralnetwork/Main.java
+++ src/main/java/net/neuralnetwork/Main.java
@@ -2,6 +2,14 @@ package net.neuralnetwork;
 
 import static java.lang.System.out;
 
+/*
+*    INPUT  AND   OR   XOR   NOR   NAND   XNOR
+*     00     0    0     0     1      1      1
+*     01     0    1     1     0      1      0
+*     10     0    1     1     0      1      0
+*     11     1    1     0     0      0      1
+ */
+
 public class Main {
     public static void main(String[] args) {
         // inputs
