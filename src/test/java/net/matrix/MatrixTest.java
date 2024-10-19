b370566651bf723ab2fd0c57fd9da98afd08b2d8
Daniel Park <intheimageofyeshua@gmail.com>
2024-10-19 21:02:32 +0900

    Created Matrix Test

--- src/test/java/net/matrix/MatrixTest.java
+++ src/test/java/net/matrix/MatrixTest.java
@@ -1,14 +1,43 @@
 package net.matrix;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MatrixTest {
     @Test
     public void constructionTest() {
         Matrix m = new Matrix(3, 4, i -> i * 2);
-        out.println(m);
+
+        String text = m.toString();
+
+        double[] exptected = new double[12];
+
+        for (int i = 0; i < exptected.length; i++) {
+            exptected[i] = i * 2;
+        }
+
+        var rows = text.split("\n");
+
+        assertTrue(rows.length == 3);
+
+        int index = 0;
+
+        for (var row: rows) {
+            var values = row.split("\\s+");
+
+            for (var textValue: values) {
+                if (textValue.length() == 0) {
+                    continue;
+                }
+
+                var doubleValue = Double.valueOf(textValue);
+
+                assertTrue(Math.abs(doubleValue - exptected[index]) < 0.001);
+                index++;
+            }
+        }
     }
 
 }
